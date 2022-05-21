157350e9ff4396c638f90c4aa076becd513bd9e2
mmtos <js223kr92@naver.com>
2022-05-22 00:16:36 +0900

    review-feedback : private 메소드 위치 변경

--- src/main/java/lotto/game/BuyLotto.java
+++ src/main/java/lotto/game/BuyLotto.java
@@ -26,17 +26,6 @@ public class BuyLotto {
         }
     }
 
-    private boolean isAvailableBuy(){
-        if(calculateAvailableDrawCount() <= NO_MORE_AVAILABLE_DRAW){
-            throw new IllegalStateException("예산이 부족하여 더 이상 구매할 수 없습니다.");
-        }
-        return true;
-    }
-
-    private int calculateAvailableDrawCount(){
-        return availableDrawCount - manuals.size();
-    }
-
     public int autoBuyRemainingBudget() {
         int autoDrawCount = calculateAvailableDrawCount();
         for (int i = 0; i < autoDrawCount; i++) {
@@ -51,6 +40,17 @@ public class BuyLotto {
         }
     }
 
+    private boolean isAvailableBuy(){
+        if(calculateAvailableDrawCount() <= NO_MORE_AVAILABLE_DRAW){
+            throw new IllegalStateException("예산이 부족하여 더 이상 구매할 수 없습니다.");
+        }
+        return true;
+    }
+
+    private int calculateAvailableDrawCount(){
+        return availableDrawCount - manuals.size();
+    }
+
     public void checkBudget(int manualBuyCount) {
         if (manualBuyCount > availableDrawCount) {
             throw new IllegalStateException("예산이 부족하여 더 이상 구매할 수 없습니다.");
@@ -62,4 +62,6 @@ public class BuyLotto {
         lottos.addAll(autos);
         return Collections.unmodifiableList(lottos);
     }
+
+
 }
